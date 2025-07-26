4df5ffe58fcf51f5f7ce3ff7856b3d361357c161
Alex Bolduc <axbolduc@gmail.com>
2025-07-26 15:10:28 -0400

    add api key auto to create runs

--- packages/api/src/routes/runs/get.ts
+++ packages/api/src/routes/runs/get.ts
@@ -1,27 +1,37 @@
-import { Effect } from "effect";
+import { Data, Effect, Schema } from "effect";
 import { Hono } from "hono";
 import { Env } from "../..";
 import { RunsService, RunsServiceLive } from "@jimbostats/core/services";
 import { effectValidator } from "@hono/effect-validator";
-import { CreateRunRequestSchema } from "./schemas";
 import { DrizzleFactory } from "@jimbostats/core/db";
+import { CreateRunSchema } from "@jimbostats/core/schemas";
 
 export const app = new Hono<Env>();
+
+class NoApiKeyError extends Data.TaggedError("NoApiKeyError")<{
+  message: string;
+}> { }
 
 app.get("/", async (c) => {
   return c.json({ t: "Hello world" });
 });
 
 // NOTE: Create run endpoint POST /runs
-app.post("/", effectValidator("json", CreateRunRequestSchema), async (c) => {
+app.post("/", effectValidator("json", CreateRunSchema), async (c) => {
   const program = Effect.gen(function* () {
+    const authHeader = c.req.header("Authorization");
+    const apiKey = authHeader?.split(" ")[1];
+
+    if (!apiKey) {
+      return yield* Effect.fail(
+        new NoApiKeyError({ message: "No api key provided" }),
+      );
+    }
+
     const runsService = yield* RunsService;
     const runRequest = c.req.valid("json");
 
-    const createdRun = yield* runsService.createRun(
-      runRequest.apiKey,
-      runRequest.run,
-    );
+    const createdRun = yield* runsService.createRun(apiKey, runRequest);
 
     return c.json(createdRun);
   }).pipe(
@@ -33,6 +43,8 @@ app.post("/", effectValidator("json", CreateRunRequestSchema), async (c) => {
             return c.json({ error: "Database error" }, 500);
           case "UserNotFoundError":
             return c.json({ error: "User not found" }, 404);
+          case "NoApiKeyError":
+            return c.json({ error: "No api key provided" }, 401);
           default:
             return c.json({ error: "Unknown error" }, 500);
         }
