5f511f14041ca84ba2fa509876564ef777b0821d
Alex Bolduc <axbolduc@gmail.com>
2025-07-26 14:37:33 -0400

    add migrations for seeding tables

---
+++ migrations/0001_seed-jokers.sql
@@ -0,0 +1,47 @@
+-- Custom SQL migration file, put your code below! --
+INSERT INTO Blinds VALUES ("5eecd2da-b659-42ca-a73a-ec8f8601b4a6", "Small Blind");
+INSERT INTO Blinds VALUES ("3a3bcfb1-d656-4a88-837a-6cc657690e55", "Big Blind");
+INSERT INTO Blinds VALUES ("9aae6bc4-5358-4264-9422-2d97a49efc5c", "The Ox");
+INSERT INTO Blinds VALUES ("9169b8c4-b61a-4e9d-9fa6-e667192feda0", "The Hook");
+INSERT INTO Blinds VALUES ("2313fbc4-e74c-4f3e-a7d6-2b8d1464fb81", "The Mouth");
+INSERT INTO Blinds VALUES ("c9652fee-0baa-4765-9238-39d036872851", "The Fist");
+INSERT INTO Blinds VALUES ("c225151b-521c-4398-a7b0-ffe0a13800dc", "The Club");
+INSERT INTO Blinds VALUES ("741c7b20-cbd7-4869-99c9-1c0e2760a30b", "The Manacle");
+INSERT INTO Blinds VALUES ("89964386-2206-46f3-b8fd-ab7713323928", "The Tooth");
+INSERT INTO Blinds VALUES ("f2f2b666-3ce6-455e-9ba8-76d2a359a073", "The Wall");
+INSERT INTO Blinds VALUES ("764166c7-7909-4a28-a93e-1f68fe3b3b9f", "The House");
+INSERT INTO Blinds VALUES ("cb3fb49c-b3d9-4721-9774-e72cd9928414", "The Mark");
+INSERT INTO Blinds VALUES ("d1902144-0d56-4374-a7fb-e0935bb00012", "Cerulean Bell");
+INSERT INTO Blinds VALUES ("9a0d319d-ea4a-4ce2-88ee-51edaae162a4", "The Wheel");
+INSERT INTO Blinds VALUES ("1eb8ad53-e5a6-49bf-a72a-65014fe816fd", "The Arm");
+INSERT INTO Blinds VALUES ("b3458491-76bc-470c-910b-e187d8f0c0d7", "The Psychic");
+INSERT INTO Blinds VALUES ("440edb20-0ed1-4340-a6d9-a4658445d99e", "The Goad");
+INSERT INTO Blinds VALUES ("4c42471a-ea06-4300-8920-8f8a76100b7a", "The Water");
+INSERT INTO Blinds VALUES ("80f438bb-13a2-4e02-b304-b40183050b7c", "The Eye");
+INSERT INTO Blinds VALUES ("934a9b8a-7a03-491a-b530-4b45f99f362e", "The Plant");
+INSERT INTO Blinds VALUES ("cb9db61f-22b4-4077-a6a4-945fd4c9bf7b", "The Needle");
+INSERT INTO Blinds VALUES ("378f5ade-766d-4209-b4a4-652bed699047", "The Head");
+INSERT INTO Blinds VALUES ("2a1c9dac-7009-4ea0-8c8e-fb02baa7a434", "Verdant Leaf");
+INSERT INTO Blinds VALUES ("9a6e4243-3131-4e19-99d7-17607eb8f118", "Violent Vessel");
+INSERT INTO Blinds VALUES ("6e87d792-1f4f-4c69-b4e6-4a370b95d58f", "The Window");
+INSERT INTO Blinds VALUES ("e65ed482-3072-4a5f-ada1-5688a704e80c", "The Serpent");
+INSERT INTO Blinds VALUES ("fe438fba-f899-4218-8c8d-b84d63561768", "The Pillar");
+INSERT INTO Blinds VALUES ("71fc8601-5beb-4669-88c0-30a916f56699", "The Flint");
+INSERT INTO Blinds VALUES ("f7f5eefc-a7bf-4611-afeb-4d5f977e3faa", "Amber Acorn");
+INSERT INTO Blinds VALUES ("a8fdc54a-69d9-454a-af95-ac2050d1ac41", "Crimson Heart");
+INSERT INTO Blinds VALUES ("ebfa1099-18b5-4c21-9bf9-6d31094b6baa", "test");
+
+
+INSERT INTO Hands VALUES ("6bf7e787-c02c-4177-b2b0-a71964fad752", "High Card", 1);
+INSERT INTO Hands VALUES ("fd719ae4-d50e-49e0-b926-df2304e30e63", "Pair", 2);
+INSERT INTO Hands VALUES ("c258f4f1-ad41-440b-8846-e7d7c88e6aa0", "Two Pair", 3);
+INSERT INTO Hands VALUES ("c5248865-dcdb-41a0-b24c-e9cfde0aa1ec", "Three of a Kind", 4);
+INSERT INTO Hands VALUES ("9cb392ac-1c1a-4a9f-aa8b-2ef583636ab4", "Straight", 5);
+INSERT INTO Hands VALUES ("75cd6a88-8202-4cc4-9acf-91d3e79c0560", "Flush", 6);
+INSERT INTO Hands VALUES ("3b1eb992-08ce-483d-933f-4cc2fbe030b2", "Full House", 7);
+INSERT INTO Hands VALUES ("12cbb658-63ad-4c4a-829a-a1bbdf7c8893", "Four of a Kind", 8);
+INSERT INTO Hands VALUES ("4b221f6b-5616-4e2b-9689-6332631c7e2f", "Straight Flush", 9);
+INSERT INTO Hands VALUES ("2c0202e2-e3a9-4162-bb78-5f4517763594", "Five of a Kind", 10);
+INSERT INTO Hands VALUES ("dd4f8ab1-40f4-4e8c-83f9-0ad441bd2f24", "Flush House", 11);
+INSERT INTO Hands VALUES ("470b1637-39e5-460b-a7ed-7f2e027de969", "Flush Five", 12);
+
